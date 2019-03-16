489cbbc525ac4d7936e9f8ff78d8c58be9a26de6
Fernando David Romero Nava <ferdav19@gmail.com>
2019-03-15 19:21:22 -0600

    Se añadieron los métodos addFunc y addVariable a DirFunc.py

--- testingCode/DirFunc.py
+++ testingCode/DirFunc.py
@@ -1,28 +1,46 @@
+import sys
+
 #Tabla de Directorio de Funciones
 class DirFunc:
     class __DirFunc:
-        def __init__(self,nombre,tipo,scope):
-            self.val = { nombre : (tipo, scope, {}) }
+        def __init__(self,nombre,tipo):
+            self.val = { nombre : (tipo, {}) }
         def __str__(self):
             return repr(self)
     instance = None
-    def __init__(self,nombre,tipo,scope):
+    def __init__(self,nombre,tipo):
         if not DirFunc.instance:
-            DirFunc.instance = DirFunc.__DirFunc(nombre,tipo,scope)
+            DirFunc.instance = DirFunc.__DirFunc(nombre,tipo)
         else:
-            DirFunc.instance.val[nombre] = (tipo, scope, {} )
+            addFunc(nombre,tipo)
     def __getattr__(self, table):
         return getattr(self.instance, table)
 
-    def addVariables(self, nameFunc, variables):
-        """ Adjunta las variables a la función especificada 
+    def addFunc(self, nombre, tipo):
+        """ Adjunta la función especificada al Directorio de Funciones 
+            Args:
+             nombre : Nombre de la función a añadir.
+             tipo   : Tipo de dato que le pertenece a la función.
+        """
+        if (nombre in DirFunc.instance.val):
+            print('Function {} previously defined.'.format(nombre))
+            sys.exit()
+        else:
+            DirFunc.instance.val[nombre] = (tipo, {} )
+
+    def addVariable(self, nameFunc, variable, tipo, filas, columnas):
+        """ Adjunta la variable a la función especificada 
             Args:
              nameFunc   : Nombre de la función a la que se le añadirán las variables.
-                            Tiene que estar en formato 'nameFunc'
-             variables  : Dictionary que contiene todas las variables encontradas.
-                            Tiene que estar en formato { nombreVariable1 : (tipoDeDato1, Scope1), nombreVariable2 : (tipoDeDato2, Scope2) ...}
+             variable   : Nombre de la variable a añadir.
+             tipo       : Tipo de dato que le pertenece a 'variable'.
+             filas      : Cantidad de filas en la variable.
+             columnas   : Cantidad de columnas en la variable.
         """
-        self.val[nameFunc] =  (self.val[nameFunc][0], self.val[nameFunc][1], variables)
-        #print('addVariables: ')
-        #print(self.val)
-        #return self
+        if (variable in self.val[nameFunc][1]):
+            print('Variable {} previously defined.'.format(variable))
+            sys.exit()
+        else:
+            tableVars = self.val[nameFunc][1]  
+            tableVars[variable] = tipo
+            self.val[nameFunc] =  (self.val[nameFunc][0], tableVars, filas, columnas)
--- testingCode/singleton.py
+++ testingCode/singleton.py
@@ -1,47 +1,19 @@
 from DirFunc import *
 
-class Vars:
-    # class __Vars:
-    #     def __init__(self,key,value):
-    #         self.val = {key: value}
-    #     def __str__(self):
-    #         return repr(self)
-    # instance = None
-    def __init__(self, key, value):
-        if not Vars.instance:
-            Vars.instance = Vars.__Vars(key,value)
-        else:
-            Vars.instance.val[key] = value
-    def __getattr__(self, table):
-        return getattr(self.instance, table)
 
-
-
-x = DirFunc('global', 'int', 'scope')
+x = DirFunc('global', 'int')
 print(list(x.val))
-y = DirFunc('func1', 'float', 'scope')
-"""
-y = DirFunc('dav', 1996)
-print(list(y.val))
-z = DirFunc('rom', 1978)
-print(list(z.val))
-w = DirFunc('fer', 1998)
-print(list(w.val))
-"""
+x.addFunc('func1', 'float')
+x.addFunc('func2', 'float')
 print(x.val)
-variables = {'fNum': ('float', 'scope'), 'fProb': ('float', 'scope')}
-x.addVariables('global', variables)
-#x.val['global'] = (x.val['global'][0], x.val['global'][1], {'fNum': ('float', 'scope')})
+x.addVariable('global', 'fNum', 'FLOAT', 1, 2)
+
 print(x.val)
-print(x.val['global'][2])
+print(x.val['global'][1])
 
-variables2 = {'iNum': ('int', 'scope'), 'iProb': ('int', 'scope')}
-x.addVariables('global', variables2)
+x.addVariable('global', 'iNum', 'INT', 2, 2)
+x.addVariable('func1', 'fNum', 'FLOAT', 1, 1)
 print(x.val)
-print(x.val['global'][2])
-
-
+print(x.val['global'][1])
 
 print(list(x.val))
-#print(y.val)
-#print(z.val)
